a7bfe25b3e4e5e2072f124b7b4a87a987d16c8a9
TheStormxd <84267004+TheStormxd@users.noreply.github.com>
2024-06-10 22:01:34 +0700

    Add files via upload
    add new updated UI

---
+++ src/infrastructure/theme/colors.js
@@ -0,0 +1,29 @@
+export const colors = {
+  brand: {
+    primary: "#2182BD",
+    secondary: "#5282BD",
+    muted: "#C6DAF7",
+  },
+  ui: {
+    primary: "#FFFFFF",  // note color
+    secondary: "#757575",
+    other: "#f5f5f5",
+    tertiary: "#F1F1F1",
+    quaternary: "#FFFFFF",
+    disabled: "#DEDEDE",
+    error: "#D0421B",
+    success: "#138000",
+  },
+  bg: {
+    primary: "#f4f4f4", //background color
+    secondary: "#FFFFFF", // header of edit note
+  },
+  text: {
+    primary: "black",    // overall text of the app
+    secondary: "#A5A5A5",  // paragraph of the note
+    disabled: "#828282",  // search note, time
+    inverse: "#FFFFFF",
+    error: "#D0421B",
+    success: "#138000",
+  },
+};
---
+++ src/services/notes/localNotes.json
@@ -0,0 +1,16 @@
+// src/services/notes/localNotes.js
+
+export const localNotes = [
+    {
+      id: 1,
+      title: "First Note",
+      content: "This is the content of the first note.",
+      date: "2023-06-07T10:00:00Z",
+    },
+    {
+      id: 2,
+      title: "Second Note",
+      content: "This is the content of the second note.",
+      date: "2023-06-07T11:00:00Z",
+    },
+  ];
